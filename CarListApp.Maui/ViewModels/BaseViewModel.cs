b7e0c857828b6c5801dae7cc07e53eddd3df6374
user <user@DESKTOP-AKN89LM>
2025-12-20 23:55:31 +0200

    Car List App
    Add MVVM base classes and update dependencies
    
    - Added BaseEntity and refactored Car to inherit from it
    - Introduced BaseViewModel and CarListViewModel using CommunityToolkit.Mvvm
    - Implemented observable properties and async car list command
    - Updated project dependencies and removed ViewModels folder entry

---
+++ CarListApp.Maui/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CarListApp.Maui.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CarListApp.Maui.ViewModels
+{
+
+    
+        public partial class BaseViewModel : ObservableObject
+        {
+            [ObservableProperty]
+            string title;
+
+            [ObservableProperty]
+            [NotifyPropertyChangedFor(nameof(IsNotLoading))]
+            bool isLoading;
+
+            public bool IsNotLoading => !isLoading;
+        
+        }
+}
---
+++ CarListApp.Maui/ViewModels/CarListViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using CarListApp.Maui.Models;
+using CarListApp.Maui.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CarListApp.Maui.ViewModels
+{
+    public partial class CarListViewModel : BaseViewModel
+    {
+        private readonly CarService carService;
+
+        public ObservableCollection<Car> Cars { get; private set; } = new ();
+
+        public CarListViewModel(CarService carService)
+        {
+            Title = "Test";
+            this.carService = carService;
+        }
+
+        [RelayCommand]
+        async Task GetCarList()
+        {
+            if(IsLoading) return;
+            try
+            {
+                IsLoading = true;
+                if(Cars.Any()) Cars.Clear();
+
+                var cars = carService.GetCars();
+                foreach (var car in cars) 
+                {
+                    Cars.Add(car);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to get car list: {ex.Message}");
+                throw;
+                await Shell.Current.DisplayAlert("Error", "Failed to retrieve list of cars. ", "OK");
+          
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+    }
+}
