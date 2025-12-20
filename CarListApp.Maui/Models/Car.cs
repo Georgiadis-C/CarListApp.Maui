4b531c659e1dd530f3bd2b82656f223e0d81a205
user <user@DESKTOP-AKN89LM>
2025-12-20 19:34:29 +0200

    Add project files.

---
+++ CarListApp.Maui/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarListApp.Maui.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Vin { get; set; }
+
+    }
+}
---
+++ CarListApp.Maui/Services/CarService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using CarListApp.Maui.Models;
+
+namespace CarListApp.Maui.Services
+{
+    public class CarService
+    {
+        public List<Car> GetCars() {
+            return new List<Car>
+            {
+                new Car { Id = 1, Make = "Toyota", Model = "Camry", Vin = "1HGCM82633A123456" },
+                new Car { Id = 2, Make = "Honda", Model = "Accord", Vin = "1HGCM82633A654321" },
+                new Car { Id = 3, Make = "Ford", Model = "Mustang", Vin = "1FAFP404X1F123456" },
+                new Car { Id = 4, Make = "Chevrolet", Model = "Malibu", Vin = "1G1ZD5ST0JF123456" },
+                new Car { Id = 5, Make = "Nissan", Model = "Altima", Vin = "1N4AL3AP7JC123456" }
+
+            };
+
+
+        }
+    }
+}
