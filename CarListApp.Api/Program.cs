f347c0c6281801b8a14433a5668a8ee5918de5ee
user <chrisg.e.o.rgiadis420@gmail.com>
2026-01-26 15:38:50 +0200

    Implement full CRUD API for /cars and update database
    Added GET, POST, PUT, and DELETE endpoints for /cars to support full CRUD operations. Fixed GET /cars/{id} to return a single car by ID. Updated SQLite database files to reflect new and modified data.

--- CarListApp.Api/Program.cs
+++ CarListApp.Api/Program.cs
@@ -1,4 +1,5 @@
 
+using System.Runtime.ConstrainedExecution;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
@@ -38,7 +39,40 @@ namespace CarListApp.Api
             app.UseAuthorization();
 
             app.MapGet("/cars", async (CarListDbContext db) => await db.Cars.ToListAsync());
-            app.MapGet("/cars/{id}", async (CarListDbContext db) => await db.Cars.ToListAsync());
+            app.MapGet("/cars/{id}", async (int id, CarListDbContext db) =>
+                await db.Cars.FindAsync(id) is Car car ? Results.Ok(car) : Results.NotFound()
+            );
+
+            app.MapPut("/cars/{id}", async (int id, Car car, CarListDbContext db) => {
+                var record = await db.Cars.FindAsync(id);
+                if (record is null) return Results.NotFound();
+
+
+                record.Make = car.Make;
+                record.Model = car.Model;
+                record.Vin = car.Vin;
+
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
+            });
+
+            app.MapDelete("/cars/{id}", async (int id, CarListDbContext db) => {
+                var record = await db.Cars.FindAsync(id);
+                if (record is null) return Results.NotFound();
+                db.Cars.Remove(record);
+
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
+            });
+
+            app.MapPost("/cars", async (int id, Car car, CarListDbContext db) => {
+                await db.AddAsync(car);
+                await db.SaveChangesAsync();
+
+                return Results.Created($"/cars/{car.Id}", car);
+            });
 
             app.Run();
         }
